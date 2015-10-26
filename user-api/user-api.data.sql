2cb305634e26c261b6fab2425f45e21ee34a440d
training <training@chucks-MacBook-Pro.local>
2015-10-26 13:13:26 -0400

    fixing java version + more data

--- user-api/user-api.data.sql
+++ user-api/user-api.data.sql
@@ -1,1 +1,45 @@
-insert into clients values (1,"Bob","Leponge","H2R 1Y9");
+insert into clients values (1,"Bob","Leponge","H1A 5J4");
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (1,1, 'Book 01',12.99,'2015-01-05');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (2,1, 'Book 02',07.99,'2014-01-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (3,1, 'Book 03',06.99,'2013-03-01');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (4,1, 'Book 04',33.99,'2014-04-07');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (5,1, 'Book 05',06.99,'2014-05-22');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (6,1, 'Book 06',05.99,'2015-05-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (7,1, 'Book 07',14.99,'2012-03-17');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (8,1, 'Book 08',12.99,'2011-02-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (9,1, 'Book 09',04.99,'2012-04-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (10,1,'Book 10',34.99,'2013-11-02');
+
+insert into clients values (2,"Jack","Hamilton","H1C 1X4");
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (11,2, 'Book 01',12.99,'2013-01-05');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (12,2, 'Book 02',07.99,'2012-11-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (13,2, 'Book 03',06.99,'2012-03-01');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (14,2, 'Book 06',05.99,'2015-07-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (15,2, 'Book 07',14.99,'2012-03-17');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (16,2, 'Book 09',04.99,'2014-06-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (17,2,'Book 10',34.99,'2011-01-02');
+
+
+insert into clients values (3,"Mary","Lewis","H2P 1R3");
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (18,3, 'Book 03',06.99,'2015-03-01');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (19,3, 'Book 04',33.99,'2011-04-07');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (20,3, 'Book 06',05.99,'2013-05-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (21,3, 'Book 08',12.99,'2014-02-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (22,3, 'Book 09',04.99,'2012-04-18');
+
+
+insert into clients values (4,"Robert","McDouglas","H1P 2X6");
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (23,4, 'Book 01',12.99,'2013-07-05');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (24,4, 'Book 02',07.99,'2014-07-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (25,4, 'Book 03',06.99,'2014-02-01');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (26,4, 'Book 04',33.99,'2015-03-07');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (27,4, 'Book 09',04.99,'2011-05-18');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (28,4,'Book 10',34.99,'2012-06-02');
+
+insert into clients values (5,"Jenny","Randall","H2K 1K2");
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (29,5, 'Book 01',12.99,'2011-11-05');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (30,5, 'Book 02',07.99,'2011-05-12');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (31,5, 'Book 04',33.99,'2013-02-07');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (32,5, 'Book 05',06.99,'2015-11-22');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (33,5, 'Book 07',14.99,'2015-09-17');
+insert into purchases(id,client_id,itemName,price,purchaseDate) values (34,5, 'Book 08',12.99,'2014-08-12');
